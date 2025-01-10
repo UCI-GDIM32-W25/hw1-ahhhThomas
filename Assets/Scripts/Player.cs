22e75998d2e0133ef311da5bdcae82df9ba88ca6
ahhhThomas <146419438+ahhhThomas@users.noreply.github.com>
2025-01-10 13:19:20 -0800

    game should work
    speed/movement slightly slow compared to hw1 example though

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,47 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.Translate(Vector2.up * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.Translate(Vector2.left * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.Translate(Vector2.down * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.Translate(Vector2.right * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_numSeedsLeft > 0)
+            {
+                PlantSeed();
+            }
+        }
     }
 
     public void PlantSeed ()
     {
-        
+            Instantiate(_plantPrefab, _playerTransform.position, transform.rotation);
+            _numSeedsLeft -= 1;
+            _numSeedsPlanted += 1;
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
